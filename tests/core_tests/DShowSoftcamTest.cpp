ffbb7fd9c77033b16f02bd9ffc742f712ad250b2
tshino <tshino.ssr@gmail.com>
2020-11-22 23:36:43 +0900

    Add tests for COM interfaces

--- tests/core_tests/DShowSoftcamTest.cpp
+++ tests/core_tests/DShowSoftcamTest.cpp
@@ -5,6 +5,8 @@
 namespace {
 namespace sc = softcam;
 
+
+// This is a dummy GUID representing nothing.
 // {12A54BBA-9F51-41B0-B331-0C3B08D1269F}
 const GUID SOME_GUID =
 { 0x12a54bba, 0x9f51, 0x41b0, { 0xb3, 0x31, 0xc, 0x3b, 0x8, 0xd1, 0x26, 0x9f } };
@@ -29,10 +31,35 @@ TEST(Softcam, QueryInterface)
 
     {
         void *ptr = nullptr;
+        hr = softcam->QueryInterface(IID_IBaseFilter, &ptr);
+        EXPECT_EQ( hr, S_OK );
+        ASSERT_NE( ptr, nullptr );
+    }{
+        void *ptr = nullptr;
+        hr = softcam->QueryInterface(IID_IAMovieSetup, &ptr);
+        EXPECT_EQ( hr, S_OK );
+        ASSERT_NE( ptr, nullptr );
+    }{
+        void *ptr = nullptr;
         hr = softcam->QueryInterface(IID_IAMStreamConfig, &ptr);
         EXPECT_EQ( hr, S_OK );
         ASSERT_NE( ptr, nullptr );
     }
+
+    softcam->Release();
+}
+
+TEST(Softcam, NoServer)
+{
+    HRESULT hr = 555;
+    sc::Softcam* softcam = (sc::Softcam*)sc::Softcam::CreateInstance(nullptr, SOME_GUID, &hr);
+    ASSERT_NE( softcam, nullptr );
+
+    EXPECT_EQ( softcam->getFrameBuffer(), nullptr );
+    EXPECT_EQ( softcam->valid(), false );
+    EXPECT_EQ( softcam->width(), 0 );
+    EXPECT_EQ( softcam->height(), 0 );
+    EXPECT_EQ( softcam->framerate(), 0.0f );
 
     softcam->Release();
 }
