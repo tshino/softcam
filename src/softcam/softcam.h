4b6fbe4f21be735ee05c862d92ffc6d809253318
tshino <tshino.ssr@gmail.com>
2020-12-25 23:49:04 +0900

    Update API description

--- src/softcam/softcam.h
+++ src/softcam/softcam.h
@@ -62,6 +62,9 @@ extern "C"
 
         If the `timeout` argument is greater than 0, this function timeouts
         after the specified time if no application accesses the virtual camera.
+
+        This function returns `true` if any application has accessed the virtual
+        camera. Otherwise, this function returns `false`.
     */
     bool        scWaitForConnection(scCamera camera, float timeout = 0.0f);
 }
