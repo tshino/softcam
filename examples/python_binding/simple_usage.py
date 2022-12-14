ef70bf2cf5e35c7960aa2c24b48bd5af6752ce2e
tshino <tshino.ssr@gmail.com>
2022-12-14 22:52:13 +0900

    Add python sample code

---
+++ examples/python_binding/simple_usage.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+from x64.Release import softcam
+
+
+class DrawSomething:
+    def __init__(self):
+        self.t = 0
+        self.img = np.zeros((240, 320, 3), dtype=np.uint8)
+
+    def update(self, dt):
+        self.t += dt
+        x = int(160 + 120*np.sin(self.t*1.5 + 0.8*np.sin(self.t*0.5)))
+        y = int(120 + 90*np.sin(self.t*1.9 + 0.8*np.sin(self.t*0.6)))
+        r = int(128 + 96*np.sin(self.t*0.3))
+        g = int(128 + 96*np.sin(self.t*0.4))
+        b = int(128 + 96*np.sin(self.t*0.5 + 1.0))
+        cv2.circle(self.img, (x,y), 10, (b,g,r), thickness=-1, lineType=cv2.LINE_AA)
+        self.img = cv2.GaussianBlur(self.img, (11,11), 0)
+        return self.img
+
+
+def main():
+    dt = 1/60
+    cam = softcam.camera(320, 240, 60)
+    draw_something = DrawSomething()
+
+    while True:
+        # draw something
+        img = draw_something.update(dt)
+        # send the image
+        cam.send_frame(img)
+
+
+if __name__ == '__main__':
+    main()
