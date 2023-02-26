e5bd393ecdb1bef91a0d336245c8b5b30849581d
tshino <tshino.ssr@gmail.com>
2023-02-26 14:30:40 +0900

    Split LaunchVisualStudio.bat

--- examples/python_binding/LaunchVisualStudio.bat
+++ examples/python_binding/LaunchVisualStudio.bat
@@ -7,12 +7,7 @@ setlocal
 
 pushd %~dp0
 
-python -c "import sys;print(sys.prefix)" > _python_dir.txt
-set /p PYTHON_DIR=<_python_dir.txt
-del _python_dir.txt
-
-set PYTHON_INCLUDE_DIR=%PYTHON_DIR%\include
-set PYTHON_LIBS_DIR=%PYTHON_DIR%\libs
+call _GetPythonPath.bat
 
 start python_binding.sln
 
