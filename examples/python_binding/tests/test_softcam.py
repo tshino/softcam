254bab38345cdc3c7a2f39a7ca7f0d00b77ade5a
tshino <tshino.ssr@gmail.com>
2023-02-27 23:55:11 +0900

    Add tests

--- examples/python_binding/tests/test_softcam.py
+++ examples/python_binding/tests/test_softcam.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from x64.Release import softcam
 
 
@@ -7,11 +8,11 @@ def test_camera_normalcase():
 
     cam = softcam.camera(320, 240, 60)
     assert cam is not None
-    assert cam.delete() is None
+    cam = None
 
     cam = softcam.camera(1920, 1080, 30)
     assert cam is not None
-    assert cam.delete() is None
+    cam.delete()
 
 
 def test_camera_multiple_instancing_fails():
@@ -66,6 +67,47 @@ def test_camera_too_big():
         softcam.camera(32000, 240, 60)
     with pytest.raises(RuntimeError):
         softcam.camera(320, 24000, 60)
+
+
+def test_camera_double_free():
+    cam = softcam.camera(320, 240, 60)
+    cam.delete()
+    cam.delete() # no effect
+
+
+def test_send_frame_normalcase():
+    cam = softcam.camera(320, 240, 60)
+    cam.send_frame(np.zeros((240, 320, 3), dtype=np.uint8))
+    cam.delete()
+
+
+def test_send_frame_invalid_dimension():
+    cam = softcam.camera(320, 240, 60)
+    with pytest.raises(ValueError):
+        cam.send_frame(np.zeros((1, 240, 320, 3), dtype=np.uint8))
+    with pytest.raises(ValueError):
+        cam.send_frame(np.zeros((240, 320), dtype=np.uint8))
+    with pytest.raises(ValueError):
+        cam.send_frame(np.zeros((240, 320, 1), dtype=np.uint8))
+    with pytest.raises(ValueError):
+        cam.send_frame(np.zeros((320, 320, 3), dtype=np.uint8))
+    with pytest.raises(ValueError):
+        cam.send_frame(np.zeros((240, 240, 3), dtype=np.uint8))
+    cam.delete()
+
+
+def test_send_frame_use_after_free():
+    cam = softcam.camera(320, 240, 60)
+    cam.delete()
+    with pytest.raises(RuntimeError) as e:
+        cam.send_frame(np.zeros((240, 320, 3), dtype=np.uint8))
+    assert e.value.args == ('the camera instance has been deleted',)
+
+
+def test_wait_for_connection():
+    cam = softcam.camera(320, 240, 60)
+    assert cam.wait_for_connection(0.01) == False
+    cam = None
 
 
 # TODO: add more tests
