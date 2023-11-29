0385ebe2b5dae14fcadfc2b6ca645b61d22a48f2
tshino <tshino.ssr@gmail.com>
2023-11-30 02:32:11 +0900

    Duplicate project files with _vs2019 suffix to keep VS2019 support

---
+++ examples/python_binding/LaunchVisualStudio_vs2019.bat
@@ -0,0 +1,15 @@
+@echo off
+@REM Call this batch with your target python environment active
+@REM to launch Visual Studio with appropriate environment variable values.
+@REM - PYTHON_INCLUDE_DIR
+@REM - PYTHON_LIBS_DIR
+setlocal
+
+pushd %~dp0
+
+call _GetPythonPath.bat
+
+start python_binding_vs2019.sln
+
+popd
+endlocal
